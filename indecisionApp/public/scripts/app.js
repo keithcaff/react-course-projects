5cdd4c7b19d9c82e5f3a5c7a6148a55652f90ca4
Caffrey <keithcaffrey@gmail.com>
2018-08-09 15:17:56 +0100

    JS expressions

--- indecisionApp/public/scripts/app.js
+++ indecisionApp/public/scripts/app.js
@@ -2,12 +2,71 @@
 
 console.log('app.js is running');
 
-var template = React.createElement(
-  'h1',
-  null,
-  ' Chaned app.js again77!'
-);
-var appRoute = document.getElementById('app');
+var appDeets = {
+    title: 'Indecision App - hello world :/',
+    subtitle: 'React app dev'
+};
 
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        appDeets.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        appDeets.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var user = {
+    name: 'Jim',
+    age: '33',
+    location: 'Galway'
+};
+
+var userName = 'Keith';
+var userAge = '30';
+var userLocation = 'NYC';
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name + '!'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
+);
+
+var appRoute = document.getElementById('app');
 //JSX to render, div to render it in
 ReactDOM.render(template, appRoute);
--- indecisionApp/src/app.js
+++ indecisionApp/src/app.js
@@ -1,8 +1,41 @@
 console.log('app.js is running');
 
-var template = <h1> Chaned app.js again77!</h1>;
-var appRoute = document.getElementById('app');
+var appDeets = {
+    title: 'Indecision App - hello world :/',
+    subtitle: 'React app dev'
+}
 
+
+var template = (
+    <div>
+        <h1>{appDeets.title}</h1> 
+        <p>{appDeets.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+
+var user = {
+    name: 'Jim',
+    age: '33',
+    location: 'Galway'    
+};
+
+var userName = 'Keith';
+var userAge = '30';
+var userLocation = 'NYC';
+var template2 = (
+    <div>
+        <h1>{user.name + '!'}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+var appRoute = document.getElementById('app');
 //JSX to render, div to render it in
 ReactDOM.render(template, appRoute);
 
