c601b025be8c228537265c6c13865d36c6f82aa7
Caffrey <keithcaffrey@gmail.com>
2018-08-13 10:44:01 +0100

    es6 classes -2

--- indecisionApp/src/playground/es6-classes.js
+++ indecisionApp/src/playground/es6-classes.js
@@ -7,19 +7,74 @@ class Person {
 
     getGreeting() {
         // return 'Hi I am ' + this.name + '!' ;
-        return `Hi I am ${this.name}`;
+        return `Hi I am ${this.name}. `;
     }
 
     getDescription() {
-        return `${this.name} is ${this.age} year(s) old`
+        return `${this.name} is ${this.age} year(s) old. `
     }
 }
 
-const me = new Person('Keith Caffrey', 29);
-console.log(me.getGreeting());
-console.log(me.getDescription());
 
-const other = new Person();
-console.log(other.getGreeting());
-console.log(other.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name,age);
+        this.major = major;    
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        // return `${super.getDescription()} major:${this.major ? this.major : 'N/A'}`
+        let desc = super.getDescription();
+        if (this.hasMajor) {
+            desc += `Their major is ${this.major}`
+        } 
+        return desc;
+    }
+}
+
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name,age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += `I am visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+// const me = new Person('Keith Caffrey', 29);
+// console.log(me.getGreeting());
+// console.log();
+
+// const other = new Person();
+// console.log(other.getGreeting());
+// console.log(other.getDescription());
+
+
+// const student1 = new Student('Keith Caffrey', 29, 'Bsc Information Systems');
+// console.log(student1.getDescription());
+// console.log(student1.hasMajor());
+
+// const student2 = new Student();
+// console.log(student2.getDescription());
+// console.log(student2.hasMajor());
+
+
+const traveller1 = new Traveller('Keith Caffrey', 29, 'Dublin, Ireland');
+console.log(traveller1.getGreeting());
+
+const traveller2 = new Traveller('Frank Grimes', 26);
+console.log(traveller2.getGreeting());
+
+
 
