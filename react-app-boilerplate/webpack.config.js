3fe8ed9e33faa1ad756575790c18bf412a9f2a64
Caffrey <keithcaffrey@gmail.com>
2018-08-20 15:56:42 +0100

    creating a boliperplate react js project

---
+++ react-app-boilerplate/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+//expose an object to another file
+module.exports = {
+    entry:'./src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module : {
+        rules : [
+            {
+                test: /\.(s?css)$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+            },
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer:  {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
